8d8440ef030fa57723cd7ed95f87c09a904e521a
stud-manasbiswas <manasbiswas342@gmail.com>
2025-05-29 20:34:39 +0530

    introduction done

---
+++ lec 1 Introduction/script.js
@@ -0,0 +1,19 @@
+alert("Hello World")
+
+console.log("code is running ...")
+console.log("code is also running ...")
+
+
+var a = prompt("enter your number")
+
+console.log("your number is "+ a)
+
+var isTrue = confirm("are you sure you want to leave this page ")
+
+if(isTrue){
+    console.log("leaving the site ")
+}
+else{
+    console.log(" not leaving the site ")
+
+}
