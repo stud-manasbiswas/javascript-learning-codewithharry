8f8643765e880ffb37362c5c124b8ba4defb0872
stud-manasbiswas <manasbiswas342@gmail.com>
2025-06-02 15:34:53 +0530

    lec 4

---
+++ lec 4 Functions/script.js
@@ -0,0 +1,26 @@
+console.log("functions in js")
+
+function greetings(name){
+    console.log("Hello " + name)
+    console.log("How are you ?")
+}
+
+greetings("manas");
+greetings("Biswas");
+
+// sum 
+
+function sum(a,b){
+    console.log(a+b);
+}
+
+sum(10,20)
+sum(5,9)
+
+// using return 
+function sumWithReturn(a,b){
+    return a+b;
+}
+
+result = sumWithReturn(50,50);
+console.log(" sum of two number is " , result);
